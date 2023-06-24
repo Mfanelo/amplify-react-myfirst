2e4817b8e359ced0d05dabb62b935d856b36dcc3
Mbele <mfanelom@909c4ad195c8.ant.amazon.com>
2023-06-24 22:13:45 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Mfanelo! vele o chinche <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
