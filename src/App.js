98405cbe1bdd54127c96112e34ab0a8d245a9656
System Administrator <root@909c4ad195c8.ant.amazon.com>
2023-06-24 22:39:47 +0200

    my 2nd commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Mfanelo! vele o chinche <code>src/App.js</code> and save to reload.
+          Mfanelo! myfirst vele o chinche <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
